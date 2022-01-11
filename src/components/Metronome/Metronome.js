393b0a57c85e99cb70b36ebc23b083d4be474dae
Dave Sanders <davepaulsanders@gmail.com>
2022-01-11 12:19:29 -0500

    Metronome turns on and off, but doesn't work with range input yet.

--- src/components/Metronome/Metronome.js
+++ src/components/Metronome/Metronome.js
@@ -6,13 +6,48 @@ import "./Metronome.css";
 function Metronome() {
   const bpm = useSelector((state) => state.metronome.bpm);
   const dispatch = useDispatch();
+  // milli is bpm converted to milliseconds
+  const milli = 60000 / bpm;
+  //setting up interval, selectors, and variables for metronome
+  let interval;
+  let isRunning = false;
+  const milliString = milli.toString();
+
+  //function to make metronome flash
+  const metronomeFlash = () => {
+    const metronome = document.getElementById("metronome");
+    const startStop = document.getElementById("startStop");
+    metronome.style.animationName = "click";
+    metronome.style.animationDuration = milliString + "ms";
+    startStop.innerHTML = "Stop";
+  };
+  //function to start metronome
+  const startMetronome = () => {
+    if (!interval) {
+      interval = setInterval(metronomeFlash, milli);
+      isRunning = true;
+    }
+  };
+  const stopMetronome = () => {
+    const metronome = document.getElementById("metronome");
+    const startStop = document.getElementById("startStop");
+    metronome.style.animationName = "";
+    metronome.style.animationDuration = "";
+    startStop.innerHTML = "Start";
+    clearInterval(interval);
+    interval = null;
+    isRunning = false;
+  };
 
   const handleChange = (event) => {
     dispatch(changeBPM({ bpm: event.target.value }));
   };
   const handleClick = () => {
-    const metronome = document.getElementById("metronome");
-    metronome.style.border = "1px solid red";
+    if (isRunning === true) {
+      stopMetronome();
+    } else {
+      startMetronome();
+    }
   };
   return (
     <div className="metronome" id="metronome">
@@ -30,7 +65,7 @@ function Metronome() {
         ></input>
         <button>+</button>
       </div>
-      <button className="start-stop" onClick={handleClick}>
+      <button className="start-stop" id="startStop" onClick={handleClick}>
         Start
       </button>
       <span className="mute-sound"></span>

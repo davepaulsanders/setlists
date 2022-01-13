84279837ed7467ae950e57c3ed87e291c5dd7828
Dave Sanders <davepaulsanders@gmail.com>
2022-01-13 10:13:15 -0500

    Datalist added and json file loaded locally.  Have written request from mock server but it's not working yet.

--- src/components/setListForm/setListForm.js
+++ src/components/setListForm/setListForm.js
@@ -1,7 +1,25 @@
-import { React, useState } from "react";
+import { React, useState, useEffect } from "react";
 import { useDispatch } from "react-redux";
 import "./setListForm.css";
 import { addSong } from "../../features/setListSlice";
+
+// importing song list from JSON server
+
+const fetchSongs = async () => {
+  try {
+    const response = await fetch(
+      "https://api.jsonbin.io/b/61e03c010f639830851b73e1"
+    );
+    if (response.ok) {
+      const jsonResponse = await response.json();
+      return jsonResponse;
+    }
+    throw new Error("Request Failed!");
+  } catch (error) {
+    console.log(error);
+  }
+};
+const songs = require("../../songdata/jazz-standards.json");
 
 export const SetListForm = () => {
   const [value, setValue] = useState([]);
@@ -26,6 +44,7 @@ export const SetListForm = () => {
       <form onSubmit={onSubmit}>
         <div className="song-submit">
           <input
+            list="song-titles"
             className="form-elements"
             placeholder="Type in a song to add!"
             type="text"
@@ -34,6 +53,11 @@ export const SetListForm = () => {
             value={value}
             onChange={(event) => setValue(event.target.value)}
           ></input>
+          <datalist id="song-titles">
+            {songs.map((song) => (
+              <option year={song.Year}>{song.Standard}</option>
+            ))}
+          </datalist>
           <label className="form-elements" htmlFor="title"></label>
         </div>
         <div className="bpm-submit">

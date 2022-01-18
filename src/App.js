1217f9c3fefaa4626f596600c38bf1f75b5440d9
Dave Sanders <davepaulsanders@gmail.com>
2022-01-18 13:43:42 -0500

    Finished most of styling, adding log in page

--- src/App.js
+++ src/App.js
@@ -4,20 +4,52 @@ import { SetListForm } from "../src/components/setListForm/setListForm";
 import Metronome from "../src/components/Metronome/Metronome";
 import SetList from "../src/components/setList/setList";
 import logo from "./audio/setlists-logo.png";
+let isLoggedIn = true;
+
 export const App = () => {
-  return (
-    <div className="App">
-      <img className="logo" src={logo} alt="logo"></img>
-      <hr></hr>
-      <div className="setlist-grid">
-        <div className="setlist-actions">
-          <SetList />
-        </div>
-        <div className="metronome-container">
-          <Metronome />
+  if (isLoggedIn === false) {
+    return (
+      <div className="App">
+        <img
+          className="logo"
+          src={logo}
+          style={{ width: "25rem" }}
+          alt="setlist-creator-logo"
+        ></img>
+        <hr></hr>
+        <div className="login-container">
+          <h1 className="login-prompt">Please log in to save setlists!</h1>
+          <form>
+            <input
+              className="username"
+              type="text"
+              placeholder="Username"
+            ></input>
+            <br></br>
+            <input
+              className="password"
+              type="text"
+              placeholder="Password"
+            ></input>
+          </form>
         </div>
       </div>
-      <SetListForm />
-    </div>
-  );
+    );
+  } else {
+    return (
+      <div className="App">
+        <img className="logo" src={logo} alt="setlist-creator-logo"></img>
+        <hr></hr>
+        <div className="setlist-grid">
+          <div className="setlist-actions">
+            <SetList />
+          </div>
+          <div className="metronome-container">
+            <Metronome />
+          </div>
+        </div>
+        <SetListForm />
+      </div>
+    );
+  }
 };

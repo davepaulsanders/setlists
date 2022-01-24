16b2598d5e730107417cf2a755389eccf0b01409
Dave Sanders <davepaulsanders@gmail.com>
2022-01-24 10:29:34 -0500

    Material UI installed but not yet used.

--- src/components/setListItems/setListItems.js
+++ src/components/setListItems/setListItems.js
@@ -16,6 +16,27 @@ function SetListItems({ title, bpm, id }) {
   };
 
   return (
+    <Grid item xs={12} md={6}>
+          <Typography sx={{ mt: 4, mb: 2 }} variant="h6" component="div">
+            Icon with text
+          </Typography>
+          <Demo>
+            <List dense={dense}>
+              {generate(
+                <ListItem>
+                  <ListItemIcon>
+                    <FolderIcon />
+                  </ListItemIcon>
+                  <ListItemText
+                    primary="Single-line item"
+                    secondary={secondary ? 'Secondary text' : null}
+                  />
+                </ListItem>,
+              )}
+            </List>
+          </Demo>
+        </Grid>
+      </Grid>
     <div className="setlist-container" onClick={selectSong}>
       <li className="songs-container" id="songs-container">
         <p className="song-details" id="title">
